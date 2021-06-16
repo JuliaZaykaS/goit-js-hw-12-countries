00a9586381dca6a1690e0662bcafdd3451078532
JuliaZaykaS <sys55@mail.ru>
2021-06-16 10:00:28 +0300

    finish

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -7,7 +7,16 @@ const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
 
 function fetchCountries(searchQuery) {
     return fetch(`${BASE_URL}${searchQuery}`)
-    .then(r=>r.json())
+        .then(r => {
+            console.dir(r);
+
+            console.log(r);
+            console.log(!r.ok);
+            if (!r.ok) return null;
+
+            return r.json()
+        })
+        // .catch()
 }
 
 export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,73 +1,72 @@
 const debounce = require('lodash.debounce');
 import '@pnotify/core/dist/BrightTheme.css';
 const { defaults } = require('@pnotify/core');
-const { error} = require('@pnotify/core');
+const { error } = require('@pnotify/core');
 
 import css from './css/style.css';
 import countryCard from './template/country-card.hbs';
 import countryList from './template/country-list.hbs';
 import API from './fetchCountries.js';
 
-
 const refs = {
-    inputEl: document.querySelector('.search-input'),
-    divOneCountryEl: document.querySelector('.single-country'),
-    divListEl: document.querySelector('.country-list'),
+  inputEl: document.querySelector('.search-input'),
+  divOneCountryEl: document.querySelector('.single-country'),
+  divListEl: document.querySelector('.country-list'),
 };
 
 refs.inputEl.addEventListener('input', debounce(onInputChange, 500));
 
-clearMarkup();
-
 function onInputChange(e) {
+  clearMarkup();
 
-    if (!e.target.value) return;
+  if (!e.target.value) return;
 
-    const data = API.fetchCountries(e.target.value);
+  const data = API.fetchCountries(e.target.value);
 
-    data.then(result => renderMarkup(result))
-        .catch(err=>onFetchError(err))
-        .finally(() => refs.inputEl.value ='')
+  data
+    .then(result => {
+      if (!result) return onFetchError();
+      return renderMarkup(result);
+    })
+    .catch(onFetchError)
+    .finally(() => (refs.inputEl.value = ''));
 }
 
 function getMarkupOfSingleCountry(country) {
-    const countryMarkup = countryCard(country);
+  const countryMarkup = countryCard(country);
 
-    refs.divOneCountryEl.insertAdjacentHTML('afterbegin', countryMarkup);
-};
+  refs.divOneCountryEl.insertAdjacentHTML('afterbegin', countryMarkup);
+}
 
 function getMarkupListOfCountries(countries) {
-    const listMarkup = countryList(countries);
-    refs.divListEl.insertAdjacentHTML('afterbegin', listMarkup);
-
+  const listMarkup = countryList(countries);
+  refs.divListEl.insertAdjacentHTML('afterbegin', listMarkup);
 }
 
 function alertError() {
-    const myError = error({
-        text: "Too many matches found. Please enter a more specific query!"
-    });
-
+  const myError = error({
+    text: 'Too many matches found. Please enter a more specific query!',
+  });
 }
 
 function clearMarkup() {
-    refs.divListEl.innerHTML='';
-    refs.divOneCountryEl.innerHTML='';
+  refs.divListEl.innerHTML = '';
+  refs.divOneCountryEl.innerHTML = '';
 }
 
-function onFetchError(err) {
-    const myErrorSecond = error({
-        text: "No results were found for your request !!! Check the name is entered correctly!"
-    });
-    // alert("No results were found for your request !!! Check the name is entered correctly!")
-
+function onFetchError() {
+  const myErrorSecond = error({
+    text: 'No results were found for your request !!! Check the name is entered correctly!',
+  });
 }
-
 
 function renderMarkup(result) {
-if (result.length > 10) {
-            alertError();
-        } else if (result.length > 2 && result.length <= 10) {
-            const arrayOfCountries = result;
-            getMarkupListOfCountries(arrayOfCountries);
-        } else if (result.length === 1){getMarkupOfSingleCountry(result)}
+  if (result.length > 10) {
+    alertError();
+  } else if (result.length > 2 && result.length <= 10) {
+    const arrayOfCountries = result;
+    getMarkupListOfCountries(arrayOfCountries);
+  } else if (result.length === 1) {
+    getMarkupOfSingleCountry(result);
+  }
 }
